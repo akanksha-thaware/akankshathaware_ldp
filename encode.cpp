d977bd9b7e8bb98e02a123739c2f8e51d31d34dc
akanksha <akanksha.thaware@zemosolabs.com>
2023-03-01 16:26:31 +0530

    edited for spaced inputs

--- encode.cpp
+++ encode.cpp
@@ -4,31 +4,20 @@
 
 #include<iostream>
 #include<string>
+
 using namespace std;
 int main(){
     int i;
     string s,result;
     // let key is 4
     int key = 4;
-    cout<<"string to encrypt\n";
-    cin>>s;
+    cout<<"enter string to encrypt : \n";
+    getline(cin, s);
     for(i=0;i<s.size();i++){
         char current = s[i];
-        // if digit or special char then replace with char at ascii + key
-        if(isdigit(current) || !isalpha(current)){
-            // get its ascii value + key
-            int asc = (int)current + key;
-            //  replace current 
-            char ch = char(asc);
-            s[i] = ch;
-            result = s;
-        }
-        else
-        // use caeser cipher for characters
-        result+=(s[i]-'A'+key)%26+'A';
-  
+        result+=(s[i]-'a'+key)%26+'a';
     }
-    cout<<"\n\nencypted message is "<<result<<'\n';
+    cout<<"\n\nencypted message is : "<<result<<'\n';
     return 0;
 }
 
